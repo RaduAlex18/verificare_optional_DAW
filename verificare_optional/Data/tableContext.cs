56e384ccd5b4cdd7dd3329046706a8083b1bc2f1
Radu Alexandru <rd_alexandruradu10@yahoo.ro>
2024-01-24 19:33:01 +0200

    Add project files.

---
+++ verificare_optional/Data/tableContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace verificare_optional.Data
+{
+    public class tableContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source=(localdb)\\mssqllocaldb;Initial Catalog=verificare_optional;TrustServerCertificate=True;");
+
+            }
+        }
+    }
+}
